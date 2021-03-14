411433f768e3a6abee2f4150741fd87faba02fab
Kushal Hada <kus@tilde.team>
2021-03-14 15:04:33 -0600

    add multiple websites

--- hello.ts
+++ hello.ts
@@ -1,15 +1,25 @@
 const { firefox } = require('playwright');
 
 (async () => {
-  hello(`https://maps.google.com`);
+  await benchmark(`https://maps.google.com`);
+  await benchmark(`https://www.google.com`);
+  await benchmark(`https://myreactivematerial.web.app/`);
+  await benchmark(`https://pat.tilde.team/`);
+  await benchmark(`https://coloradoquiz.web.app/`);
+  await benchmark(`https://scottkast.github.com`);
 })();
 
-async function hello(address: string) {
-  const browser = await firefox.launch();
-  const page = await browser.newPage();
-  const start_time = +new Date();
-  await page.goto(address);
-  const end_time = +new Date();
-  console.log(`Total load time for ${address} was ${(end_time - start_time)/1000} seconds`);
-  await browser.close();
-} 
+async function benchmark(address: string) {
+  try {
+    const browser = await firefox.launch();
+    const page = await browser.newPage();
+    const start_time = +new Date();
+    await page.goto(address);
+    const end_time = +new Date();
+    console.log(`Total load time for ${address} was ${(end_time - start_time) / 1000} seconds`);
+    await browser.close();
+  } catch (error) {
+    console.log(`Error benchmarking ${address}`);
+    console.error({ error });
+  }
+}
